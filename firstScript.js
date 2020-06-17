173c7197d2258337b04bc62c53e7ab26dc4cbd9f
wcspracklin <williamspracklin@gmail.com>
2020-06-17 18:26:48 -0400

    Initial commit

---
+++ firstScript.js
@@ -0,0 +1,9 @@
+let firstParagraphVariable = document.querySelector('#firstParagraph');
+
+firstParagraphVariable.innerHTML = "JavaScript changed my page!";
+
+// github this code
+// export power point
+// get with sherry and fix emails
+// send links
+// get personal emails
