514624ee1e9d1c180063e2a0c7bb1c3e870c212c
wcspracklin <williamspracklin@gmail.com>
2020-06-30 11:18:57 -0400

    Removed comments

--- firstScript.js
+++ firstScript.js
@@ -2,8 +2,3 @@ let firstParagraphVariable = document.querySelector('#firstParagraph');
 
 firstParagraphVariable.innerHTML = "JavaScript changed my page!";
 
-// github this code
-// export power point
-// get with sherry and fix emails
-// send links
-// get personal emails
